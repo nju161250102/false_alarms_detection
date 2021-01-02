a923f49af78eb14e22f20529c47bd196205662cf
nju161250102 <161250102@smail.nju.edu.cn>
2021-01-02 19:54:32 +0800

    Change

--- python/DataSet.py
+++ python/DataSet.py
@@ -19,13 +19,9 @@ class DataLoader:
         self.compress_flag = False
         # 文件列表，格式：[(文件路径, 文件ID)...]
         self.file_list = []
-        # 标签Map，格式：{文件ID, 0/1标签}
-        self.label_dict = {}
         # 数据列表，格式：[单个数据]
         self.data_list = []
-        # 获取文件夹下文件路径与ID的对应关系
-        for f in os.listdir(data_dir):
-            self.file_list.append((os.path.join(data_dir, f), os.path.splitext(f)[0][:-7]))
+        self.data_dir = data_dir
 
     def split_data(self, rate: float) -> tuple:
         """
@@ -37,28 +33,31 @@ class DataLoader:
         train_data = list(zip(*self.data_list[:int(len(self.data_list) * rate)]))
         test_data = list(zip(*self.data_list[int(len(self.data_list) * rate):]))
         if self.compress_flag:
-            X_train, y_train, X_test, y_test = self.transform_x(list(train_data[0])), list(train_data[1]), self.transform_x(list(test_data[0])), list(test_data[1])
-        else:
-            X_train, y_train, X_test, y_test = list(train_data[0]), list(train_data[1]), list(test_data[0]), list(test_data[1])
-        if DataLoader.over_sample:
-            ros = RandomOverSampler(random_state=0)
-            X_resampled, y_resampled = ros.fit_sample(X_train, y_train)
-            print(len(X_resampled), len(X_train))
-            return X_resampled, y_resampled, X_test, y_test
-        if DataLoader.under_sample:
-            rus = RandomUnderSampler(random_state=0)
-            X_resampled, y_resampled = rus.fit_sample(X_train, y_train)
-            return X_resampled, y_resampled, X_test, y_test
-        return X_train, y_train, X_test, y_test
+            return self.transform_x(list(train_data[0])), list(train_data[1]), self.transform_x(list(test_data[0])), list(test_data[1])
+        return list(train_data[0]), list(train_data[1]), list(test_data[0]), list(test_data[1])
 
     def load_label(self, label_csv: str):
         """
         :param label_csv: 处理好的标记csv，每行格式[数据ID，标记]
-        :return: 数据ID到标记的映射
         """
         df = pd.read_csv(label_csv, header=None)
+        d = {}
         for row in df.itertuples():
-            self.label_dict[row[1]] = row[2]
+            d[row[1]] = row[2]
+        for f in os.listdir(self.data_dir):
+            file_id = os.path.splitext(f)[0][:-7]
+            if file_id not in d.keys():
+                continue
+            self.file_list.append((os.path.join(self.data_dir, f), d[file_id]))
+        X, y = zip(* self.file_list)
+        if DataLoader.over_sample:
+            ros = RandomOverSampler(random_state=0)
+            X_resampled, y_resampled = ros.fit_sample(list(X), list(y))
+            self.file_list = list(zip(X_resampled, y_resampled))
+        if DataLoader.under_sample:
+            rus = RandomUnderSampler(random_state=0)
+            X_resampled, y_resampled = rus.fit_sample(list(X), list(y))
+            self.file_list = list(zip(X_resampled, y_resampled))
 
     @staticmethod
     def transform_x(x_data):
@@ -74,9 +73,7 @@ class VectorDataLoader(DataLoader):
         super().__init__(data_dir)
 
     def read_data(self, len_limit: int):
-        for file_path, file_id in self.file_list:
-            if file_id not in self.label_dict.keys():
-                continue
+        for file_path, file_label in self.file_list:
             df = pd.read_csv(file_path, header=None)
             feature_len = df.shape[0]
             x_data = []
@@ -86,7 +83,7 @@ class VectorDataLoader(DataLoader):
             if feature_len < len_limit:
                 for i in range(feature_len, len_limit):
                     x_data.append([0 for j in range(df.shape[1])])
-            self.data_list.append((x_data, self.label_dict[file_id]))
+            self.data_list.append((x_data, file_label))
 
 
 class WordDataLoader(DataLoader):
@@ -98,15 +95,13 @@ class WordDataLoader(DataLoader):
         text_data = []
         id_list = []
         vectorizer = CountVectorizer() if handler == "Count" else TfidfVectorizer()
-        for file_path, file_id in self.file_list:
-            if file_id not in self.label_dict.keys():
-                continue
+        for file_path, file_label in self.file_list:
             df = pd.read_csv(file_path, header=None)
             text_data.append(" ".join([str(s) for s in df.values.tolist()[0]]))
-            id_list.append(file_id)
+            id_list.append(file_label)
         vector_array = vectorizer.fit_transform(text_data).toarray()
         for i in range(len(vector_array)):
-            self.data_list.append((vector_array[i], self.label_dict[id_list[i]]))
+            self.data_list.append((vector_array[i], id_list[i]))
 
 
 class ManualDataLoader(DataLoader):
@@ -115,11 +110,9 @@ class ManualDataLoader(DataLoader):
         super().__init__(data_dir)
 
     def read_data(self):
-        for file_path, file_id in self.file_list:
-            if file_id not in self.label_dict.keys():
-                continue
+        for file_path, file_label in self.file_list:
             with open(file_path) as txt_file:
-                self.data_list.append(([int(s) for s in txt_file.readline().split()[:5]], self.label_dict[file_id]))
+                self.data_list.append(([int(s) for s in txt_file.readline().split()[:5]], file_label))
 
 
 class DocDataLoader(DataLoader):
@@ -128,8 +121,6 @@ class DocDataLoader(DataLoader):
         super().__init__(data_dir)
 
     def read_data(self):
-        for file_path, file_id in self.file_list:
-            if file_id not in self.label_dict.keys():
-                continue
+        for file_path, file_label in self.file_list:
             df = pd.read_csv(file_path, header=None)
-            self.data_list.append((df.values.tolist()[0], self.label_dict[file_id]))
+            self.data_list.append((df.values.tolist()[0], file_label))
